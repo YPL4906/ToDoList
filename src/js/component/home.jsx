ffd6b9ea44021dfef27ddb80a99ddfcf270c7b2c
YPL4906 <104123424+YPL4906@users.noreply.github.com>
2025-01-08 17:18:04 +0000

    To Do List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from 'react';
+import '/workspaces/ToDoList/src/styles/index.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function home() {
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-export default Home;
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      addTask();
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+      <div className="todo-container">
+        <input
+          type="text"
+          placeholder="Tareas"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          onKeyPress={handleKeyPress}
+          className="todo-input"
+        />
+        <ul className="todo-list">
+          {tasks.map((task, index) => (
+            <li key={index} className="todo-item">
+              {task}
+              <button
+                onClick={() => deleteTask(index)}
+                className="delete-button"
+              >
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+        <p className="todo-counter">{tasks.length} item{tasks.length !== 1 ? 's' : ''} left</p>
+      </div>
+    </div>
+  );
+}
+
+export default home;
